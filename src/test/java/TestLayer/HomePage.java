df0a5224966b4be2d6a78b65af8e6334df4425d5
Suraj Patil <prafpawar11@gmail.com>
2023-03-24 09:07:00 +0530

    Home Page Test cases

---
+++ src/test/java/TestLayer/HomePage.java
@@ -0,0 +1,12 @@
+package TestLayer;
+
+public class HomePage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Home Page Test cases");
+		
+	}
+
+}
